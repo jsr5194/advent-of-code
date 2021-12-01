50f6f4fa22cacd96dcd08fe11a8b9711e157a05d
jrittle <jrittle@cisco.com>
2021-12-01 10:13:06 -0500

    ARE YOU HAPPY NOW CARL

--- 2021/aoc2021/src/day1/mod.rs
+++ 2021/aoc2021/src/day1/mod.rs
@@ -4,7 +4,7 @@ fn get_input() -> Vec<u32> {
 	let filename = "./src/day1/input.txt";
 	let contents_str = fs::read_to_string(filename).expect("[!] ERROR: an error occurred while reading the file");
 	let contents: Vec<u32> = contents_str.lines().map(|x| x.parse::<u32>().expect("[!] ERROR: could not convert to u32")).collect();
-	return contents;
+	contents
 }
 
 /// run Day 1 Part 1
