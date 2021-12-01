83a65d908098b5a7822b38af50a9675729cd2f31
jsr5194 <jsr5194@gmail.com>
2021-12-01 10:09:25 -0500

    Delete 2020/_archive/day1ex1/target directory

--- 2020/_archive/day1ex1/target/debug/day1ex1.d
+++
@@ -1,1 +0,0 @@
-/Users/jrittle_adm/src/advent-of-code/2020/day1ex1/target/debug/day1ex1: /Users/jrittle_adm/src/advent-of-code/2020/day1ex1/src/main.rs
--- 2020/_archive/day1ex1/target/debug/deps/day1ex1.d
+++
@@ -1,5 +0,0 @@
-/Users/jrittle_adm/src/advent-of-code/2020/day1ex1/target/debug/deps/day1ex1: src/main.rs
-
-/Users/jrittle_adm/src/advent-of-code/2020/day1ex1/target/debug/deps/day1ex1.d: src/main.rs
-
-src/main.rs:
