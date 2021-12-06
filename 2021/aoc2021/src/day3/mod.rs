83db5a6370293f44929d6f32657d01a9adde2da1
jrittle <jrittle@cisco.com>
2021-12-06 13:25:15 -0500

    finished day3

--- 2021/aoc2021/src/day3/mod.rs
+++ 2021/aoc2021/src/day3/mod.rs
@@ -1,9 +1,9 @@
 use std::fs;
 
-fn get_input() -> Vec<u32> {
-	let filename = "./src/day3/input_test.txt";
+fn get_input() -> DiagnosticReport {
+	let filename = "./src/day3/input.txt";
 	let contents_str = fs::read_to_string(filename).expect("[!] ERROR: an error occurred while reading the file");
-	let contents: Vec<u32> = contents_str.lines().map(|x| u32::from_str_radix(x, 2).unwrap()).collect();
+	let contents: DiagnosticReport = DiagnosticReport::from(contents_str);
 	contents
 }
 
@@ -13,18 +13,14 @@ pub fn run_part_1() {
 	let mut gamma_rate: u32 = 0;
 	let mut epsilon_rate: u32 = 0;
 
-	// set max check depth
-	let max_depth: u32 = 5;
-
 	// input
 	let report = get_input();
-	println!("contents: {:?}", report);
 
 	let mut mask: u32 = 1;
-	for i in max_depth..0 {
+	for i in 0..report.depth {
 		let mut zeroes: u32 = 0;
 		let mut ones: u32 = 0;
-		for entry in &report {
+		for entry in &report.entries {
 			if ((entry & mask) >> i) == 1 {
 				ones += 1;
 			} else {
@@ -40,58 +36,105 @@ pub fn run_part_1() {
 		mask = mask << 1;
 	}
 
-	println!("gamma_rate: {:?}", gamma_rate);
-	println!("epsilon_rate: {:?}", epsilon_rate);
-
-	// input.txt 845186
 	println!("[*] Day 3 Part 1 Result: {}", gamma_rate * epsilon_rate);
 }
 
 /// run Day 3 Part 2
 pub fn run_part_2() {
-	// define results
-	let mut o2: u32 = 0;
-	let co2: u32 = 0;
+	let full_report = get_input();
 
-	// set max check depth
-	let max_depth: u32 = 5;
+	let mut mask = 0;
 
-	// input
-	let mut report = get_input();
+	// oxygen generator rating
+	mask = 1 << full_report.depth-1;
 
-	// oxygen
-	let mut mask: u32 = 1;
-	for i in max_depth..0 {
-		let mut zeroes: u32 = 0;
-		let mut ones: u32 = 0;
-		for entry in &report {
-			if ((entry & mask) >> i) == 1 {
+	let mut cur_report = full_report.clone();
+
+	for bit_pos in (0..full_report.depth).rev() {
+		mask = 1 << bit_pos;
+		let mut zeroes = 0;
+		let mut ones = 0;
+
+		for entry in &cur_report.entries {
+			if ((entry & mask) >> bit_pos) == 1 {
 				ones += 1;
 			} else {
 				zeroes += 1;
 			}
 		}
 
-		let mut new_report: Vec<u32> = vec![];
 		if ones >= zeroes {
-			for entry in &report {
-				if ((entry & mask) >> i) == 1 {
-					new_report.push(entry.clone());
-				}
-			}
-		}
-		report = new_report.clone();
-		println!("report len {:?}", report.len());
-		if report.len() == 1 {
-			o2 = report[0];
+			cur_report.entries.retain(|&entry| (entry & mask) >> bit_pos == 1);
+		} else {
+			cur_report.entries.retain(|&entry| (entry & mask) >> bit_pos == 0);
 		}
 
-
-		mask = mask << 1;
+		if cur_report.entries.len() == 1 {
+			break;
+		}
 	}
 
-	println!("o2: {:?}", o2);
-	println!("co2: {:?}", co2);
+	if cur_report.entries.len() != 1 {
+		panic!("unexpected number of entries left: {}", cur_report.entries.len());
+	}
+	let o2_rating = cur_report.entries[0];
 
-	println!("[*] Day 3 Part 2 Result: TODO");
+
+	// carbon dioxide generator rating
+	mask = 1 << full_report.depth-1;
+
+	let mut cur_report = full_report.clone();
+
+	for bit_pos in (0..full_report.depth).rev() {
+		mask = 1 << bit_pos;
+		let mut zeroes = 0;
+		let mut ones = 0;
+
+		for entry in &cur_report.entries {
+			if ((entry & mask) >> bit_pos) == 1 {
+				ones += 1;
+			} else {
+				zeroes += 1;
+			}
+		}
+
+		if zeroes <= ones {
+			cur_report.entries.retain(|&entry| (entry & mask) >> bit_pos == 0);
+		} else {
+			cur_report.entries.retain(|&entry| (entry & mask) >> bit_pos == 1);
+		}
+
+		if cur_report.entries.len() == 1 {
+			break;
+		}
+	}
+
+	if cur_report.entries.len() != 1 {
+		panic!("unexpected number of entries left: {}", cur_report.entries.len());
+	}
+	let co2_rating = cur_report.entries[0];
+
+	println!("[*] Day 3 Part 2 Result: {}", o2_rating * co2_rating);
 }
+
+
+#[derive(Debug, Default, Clone, PartialEq)]
+struct DiagnosticReport {
+	entries: Vec<u32>,
+	depth: usize,
+}
+
+impl From<String> for DiagnosticReport {
+	fn from(report_str: String) -> Self {
+		let mut report = DiagnosticReport::default();
+		report.depth = report_str.lines().next().unwrap().chars().count();
+		for line in report_str.lines() {
+			report.entries.push(u32::from_str_radix(line, 2).unwrap())
+		}
+
+		report
+	}
+}
+
+
+
