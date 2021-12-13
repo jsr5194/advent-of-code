953e6609c4d2586378d9189cbc15e627fb9d5ee4
jrittle <jrittle@cisco.com>
2021-12-13 14:10:14 -0500

    day11 finished

--- 2021/aoc2021/src/day11/mod.rs
+++ 2021/aoc2021/src/day11/mod.rs
@@ -2,7 +2,7 @@ use std::fmt;
 use std::fs;
 
 fn get_input() -> Consortium {
-	let filename = "./src/day11/input_test_1.txt";
+	let filename = "./src/day11/input.txt";
 	let contents_str = fs::read_to_string(filename).expect("[!] ERROR: an error occurred while reading the file");
 	let contents: Consortium = Consortium::from(contents_str);
 	contents
@@ -10,25 +10,242 @@ fn get_input() -> Consortium {
 
 /// run Day 11 Part 1
 pub fn run_part_1() {
-	let consortium = get_input();
+	let mut consortium = get_input();
+	for step in 0..100 {
+		consortium.step();	
+	}
+
 	println!("{}", consortium);
-	println!("[*] Day 11 Part 1 Result: TODO");
+	
+	println!("[*] Day 11 Part 1 Result: {}", consortium.flashes);
 }
 
 /// run Day 11 Part 2
 pub fn run_part_2() {
-	println!("[*] Day 11 Part 2 Result: TODO");
+	let mut consortium = get_input();
+	let mut step = 1;
+	let mut simultaneous_flash = true;
+	loop {
+		simultaneous_flash = true;
+		consortium.step();
+		for y in 0..consortium.members.len() {
+			for x in 0..consortium.members[0].len() {
+				if consortium.members[y][x].energy != 0 {
+					simultaneous_flash = false;
+					break;
+				}
+			}
+			if simultaneous_flash {
+				break;
+			}
+		}
+		if simultaneous_flash {
+			break;
+		}
+		step += 1;
+	}
+	
+	println!("[*] Day 11 Part 2 Result: {}", step);
 }
 
 
 #[derive(Debug, Default, Clone, PartialEq)]
 struct Octopus {
-	energy: u32,
+	energy: usize,
+	flashed_this_step: bool,
 }
 
 #[derive(Debug, Default, Clone, PartialEq)]
 struct Consortium {
 	members: Vec<Vec<Octopus>>,
+	flashes: usize,
+}
+
+impl Consortium {
+	fn step(&mut self) {
+		// reset the flash state
+		for y in 0..self.members.len() {
+			for x in 0..self.members[0].len() {
+				self.members[y][x].flashed_this_step = false;
+			}
+		}
+
+		for y in 0..self.members.len() {
+			for x in 0..self.members[0].len() {
+				if self.members[y][x].energy == 9 {
+					self.flash(x, y);
+				} else {
+					if !self.members[y][x].flashed_this_step {
+						self.members[y][x].energy += 1;
+					}
+				}
+			}
+		}
+	}
+
+	fn flash(&mut self, x: usize, y:usize) {
+		self.members[y][x].energy = 0;
+		self.members[y][x].flashed_this_step = true;
+		self.flashes += 1;
+		
+		// top left
+		if x == 0 && y == 0 {
+			if !self.members[y][x+1].flashed_this_step {
+				self.members[y][x+1].energy += 1;
+			}
+			if !self.members[y+1][x].flashed_this_step {
+				self.members[y+1][x].energy += 1;
+			}
+			if !self.members[y+1][x+1].flashed_this_step {
+				self.members[y+1][x+1].energy += 1;
+			}
+		}
+		// top right
+		else if x == self.members[0].len()-1 && y == 0 {
+			if !self.members[y][x-1].flashed_this_step {
+				self.members[y][x-1].energy += 1;
+			}
+			if !self.members[y+1][x-1].flashed_this_step {
+				self.members[y+1][x-1].energy += 1;
+			}
+			if !self.members[y+1][x].flashed_this_step {
+				self.members[y+1][x].energy += 1;
+			}
+		}
+		// bottom left
+		else if x == 0 && y == self.members.len()-1 {
+			if !self.members[y-1][x].flashed_this_step {
+				self.members[y-1][x].energy += 1;
+			}
+			if !self.members[y-1][x+1].flashed_this_step {
+				self.members[y-1][x+1].energy += 1;
+			}
+			if !self.members[y][x+1].flashed_this_step {
+				self.members[y][x+1].energy += 1;
+			}
+		}
+		// bottom right
+		else if x == self.members[0].len()-1 && y == self.members.len()-1 {
+			if !self.members[y-1][x].flashed_this_step {
+				self.members[y-1][x].energy += 1;
+			}
+			if !self.members[y-1][x-1].flashed_this_step {
+				self.members[y-1][x-1].energy += 1;
+			}
+			if !self.members[y][x-1].flashed_this_step {
+				self.members[y][x-1].energy += 1;
+			}
+		}
+		// top row
+		else if y == 0 {
+			if !self.members[y][x-1].flashed_this_step {
+				self.members[y][x-1].energy += 1;
+			}
+			if !self.members[y][x+1].flashed_this_step {
+				self.members[y][x+1].energy += 1;
+			}
+			if !self.members[y+1][x-1].flashed_this_step {
+				self.members[y+1][x-1].energy += 1;
+			}
+			if !self.members[y+1][x].flashed_this_step {
+				self.members[y+1][x].energy += 1;
+			}
+			if !self.members[y+1][x+1].flashed_this_step {
+				self.members[y+1][x+1].energy += 1;
+			}
+		}
+		// left row
+		else if x == 0 {
+			if !self.members[y-1][x].flashed_this_step {
+				self.members[y-1][x].energy += 1;
+			}
+			if !self.members[y-1][x+1].flashed_this_step {
+				self.members[y-1][x+1].energy += 1;
+			}
+			if !self.members[y][x+1].flashed_this_step {
+				self.members[y][x+1].energy += 1;
+			}
+			if !self.members[y+1][x].flashed_this_step {
+				self.members[y+1][x].energy += 1;
+			}
+			if !self.members[y+1][x+1].flashed_this_step {
+				self.members[y+1][x+1].energy += 1;
+			}
+		}
+		// right row
+		else if x == self.members[0].len()-1 {
+			if !self.members[y-1][x-1].flashed_this_step {
+				self.members[y-1][x-1].energy += 1;
+			}
+			if !self.members[y-1][x].flashed_this_step {
+				self.members[y-1][x].energy += 1;
+			}
+			if !self.members[y][x-1].flashed_this_step {
+				self.members[y][x-1].energy += 1;
+			}
+			if !self.members[y+1][x].flashed_this_step {
+				self.members[y+1][x].energy += 1;
+			}
+			if !self.members[y+1][x-1].flashed_this_step {
+				self.members[y+1][x-1].energy += 1;
+			}
+		}
+		// bottom row
+		else if y == self.members.len()-1 {
+			if !self.members[y-1][x-1].flashed_this_step {
+				self.members[y-1][x-1].energy += 1;
+			}
+			if !self.members[y-1][x].flashed_this_step {
+				self.members[y-1][x].energy += 1;
+			}
+			if !self.members[y-1][x+1].flashed_this_step {
+				self.members[y-1][x+1].energy += 1;
+			}
+			if !self.members[y][x-1].flashed_this_step {
+				self.members[y][x-1].energy += 1;
+			}
+			if !self.members[y][x+1].flashed_this_step {
+				self.members[y][x+1].energy += 1;
+			}
+		}
+		// middles
+		else {
+			if !self.members[y-1][x-1].flashed_this_step {
+				self.members[y-1][x-1].energy += 1;
+			}
+			if !self.members[y-1][x].flashed_this_step {
+				self.members[y-1][x].energy += 1;
+			}
+			if !self.members[y-1][x+1].flashed_this_step {
+				self.members[y-1][x+1].energy += 1;
+			}
+			if !self.members[y][x-1].flashed_this_step {
+				self.members[y][x-1].energy += 1;
+			}
+			if !self.members[y][x+1].flashed_this_step {
+				self.members[y][x+1].energy += 1;
+			}
+			if !self.members[y+1][x-1].flashed_this_step {
+				self.members[y+1][x-1].energy += 1;
+			}
+			if !self.members[y+1][x].flashed_this_step {
+				self.members[y+1][x].energy += 1;
+			}
+			if !self.members[y+1][x+1].flashed_this_step {
+				self.members[y+1][x+1].energy += 1;
+			}
+		}
+
+		// loop over everything and make sure nothing is nine
+		for y in 0..self.members.len() {
+			for x in 0..self.members[0].len() {
+				if self.members[y][x].energy > 9 {
+					self.flash(x, y);
+				}
+			}
+		}
+
+	}
 }
 
 impl fmt::Display for Consortium {
@@ -50,7 +267,7 @@ impl From<String> for Consortium {
 		for line in consortium_str.lines() {
 			let mut cur_octopi: Vec<Octopus> = vec![];
 			for c in line.chars() {
-				cur_octopi.push(Octopus{energy: c.to_string().parse::<u32>().unwrap()});
+				cur_octopi.push(Octopus{energy: c.to_string().parse::<usize>().unwrap(), flashed_this_step: false});
 			}
 			consortium.members.push(cur_octopi);
 		}
