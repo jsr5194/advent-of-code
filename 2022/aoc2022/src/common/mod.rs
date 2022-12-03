bd50bbee86858895b6449cf7d6e526f87baa929a
Jared Rittle <>
2022-12-03 11:55:51 -0500

    aoc2022: adding trim to file read

--- 2022/aoc2022/src/common/mod.rs
+++ 2022/aoc2022/src/common/mod.rs
@@ -3,6 +3,7 @@ use std::fs;
 pub fn read_file(filename: &str) -> String {
     let filedata = fs::read_to_string(&filename)
         .expect("could not read file")
+        .trim()
         .to_string();
     filedata
 }
