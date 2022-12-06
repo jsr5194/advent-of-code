bb3a19f2b0e9e9cdd8c0f14b0c216f432e9e5473
Jared Rittle <>
2022-12-06 13:52:03 -0500

    aoc2022: day6 benchmarking

--- 2022/aoc2022/benches/aoc_benchmark.rs
+++ 2022/aoc2022/benches/aoc_benchmark.rs
@@ -87,8 +87,8 @@ macro_rules! build_bench_notrim_mut {
     };
 }
 
-build_bench_notrim_mut!(day5);
-criterion_group!(single, day5);
+build_bench!(day6);
+criterion_group!(single, day6);
 criterion_main!(single);
 
 //pub fn day1_benchmark(c: &mut Criterion) {
