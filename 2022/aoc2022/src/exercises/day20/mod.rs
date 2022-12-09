48d00e46971f8022284261b5655ed6554ac749e3
Jared Rittle <>
2022-12-08 19:01:11 -0500

    aoc2022: updating template

--- 2022/aoc2022/src/exercises/day20/mod.rs
+++ 2022/aoc2022/src/exercises/day20/mod.rs
@@ -3,23 +3,31 @@ use log::info;
 pub fn process_input(filedata: &String) {}
 
 pub fn run_part1(filedata: &String) {
-    info!("running part 1");
-    part1();
+    // process_input(filedata)
+    let result = part1();
+    info!("Day 9 Part 1 Answer: {}", result);
+    result
 }
 
-pub fn part1() {}
+pub fn part1() -> usize {
+    0
+}
 
 pub fn run_part2(filedata: &String) {
-    info!("running part 2");
-    part2();
+    // process_input(filedata)
+    let result = part1();
+    info!("Day 9 Part 2 Answer: {}", result);
+    result
 }
 
-pub fn part2() {}
+pub fn part2() -> usize {
+    0
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::exercises::day2::run_part1;
-    use crate::exercises::day2::run_part2;
+    use crate::exercises::day1::run_part1;
+    use crate::exercises::day1::run_part2;
     #[test]
     fn test() {}
 }
