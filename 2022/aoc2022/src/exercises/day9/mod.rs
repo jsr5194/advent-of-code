19f84ed4341377192594ffbe8806feec8cf51925
Jared Rittle <>
2022-12-09 16:35:31 -0500

    aoc2022: day9 solutions

--- 2022/aoc2022/src/exercises/day9/mod.rs
+++ 2022/aoc2022/src/exercises/day9/mod.rs
@@ -15,6 +15,7 @@ pub fn run_part1(filedata: &String) -> usize {
 
 pub fn part1(series: &MotionSeries) -> usize {
     let mut bridge = Bridge::default();
+    //println!("# Initial\n{}\n\n\n", bridge);
     for motion in &series.motions {
         bridge.process_motion(&motion);
     }
@@ -36,7 +37,19 @@ pub fn run_part2(filedata: &String) -> usize {
 }
 
 pub fn part2(series: &MotionSeries) -> usize {
-    0
+    let mut bridge = Bridge::default();
+    bridge.set_knots(10);
+    for motion in &series.motions {
+        bridge.process_motion(&motion);
+    }
+    let mut result = 0;
+    for (coord, plank) in &bridge.planks {
+        if plank.tail_visit_count > 0 {
+            result += 1;
+        }
+    }
+    //println!("{}", bridge);
+    result
 }
 
 #[cfg(test)]
@@ -53,6 +66,10 @@ mod tests {
         assert_eq!(
             run_part1(&read_file("./src/exercises/day9/input.txt")),
             6339
+        );
+        assert_eq!(
+            run_part2(&read_file("./src/exercises/day9/input_test_2.txt")),
+            36
         );
     }
 }
@@ -121,11 +138,21 @@ impl fmt::Display for Plank {
 #[derive(Debug)]
 struct Bridge {
     planks: HashMap<Coordinates, Plank>,
-    head: Coordinates,
-    tail: Coordinates,
+    knots: Vec<Coordinates>,
 }
 
 impl Bridge {
+    fn set_knots(&mut self, count: usize) {
+        if count < 2 {
+            panic!("must have at least two knots");
+        }
+
+        // -2 to account for the two created by default
+        let new_knot = self.knots[self.knots.len() - 1].clone();
+        for _ in 0..count - 2 {
+            self.knots.push(new_knot.clone());
+        }
+    }
     fn get_max_y(&self) -> usize {
         let mut max_y = 0;
         for key in self.planks.keys() {
@@ -145,105 +172,146 @@ impl Bridge {
         max_x
     }
 
-    fn update_tail_position(&mut self, x: usize, y: usize) {
-        self.tail.x = x;
-        self.tail.y = y;
-        if let Some(cur_plank) = self.planks.get_mut(&self.tail) {
-            cur_plank.tail_visit_count += 1;
-        } else {
+    fn update_knot_position(&mut self, knot_idx: usize, x: usize, y: usize) {
+        let tail = Coordinates::from(&format!("({},{})", x, y));
+
+        self.knots[knot_idx] = tail.clone();
+
+        if !self.planks.contains_key(&tail) {
             self.planks.insert(
-                self.tail.clone(),
+                tail.clone(),
                 Plank {
-                    position: self.tail.clone(),
-                    tail_visit_count: 1,
+                    position: tail.clone(),
+                    tail_visit_count: 0,
                 },
             );
         }
     }
 
-    fn move_tail(&mut self) {
+    fn move_trailing_knot(
+        &mut self,
+        trailing_knot_idx: usize,
+        head: &Coordinates,
+        tail: &Coordinates,
+    ) {
         // move the tail
         //   don't need to bother with the following cases:
         //     1) when the head and tail are in the sample place we don't need to do anything
         //     2) tail is already next to the head
 
-        if self.head.x == self.tail.x && self.head.y == self.tail.y - 2 {
+        if head.x == tail.x && head.y == tail.y - 2 {
             // head is up with one space between
             // H
             // .
             // T
-            self.update_tail_position(self.tail.x, self.tail.y - 1);
-        } else if self.head.x == self.tail.x + 2 && self.head.y == self.tail.y - 1 {
+            self.update_knot_position(trailing_knot_idx, tail.x, tail.y - 1);
+        } else if head.x == tail.x + 2 && head.y == tail.y - 1 {
             // head up and left with a space in between
             // ..H
             // T..
-            self.update_tail_position(self.tail.x + 1, self.tail.y - 1);
-        } else if self.head.x == self.tail.x + 1 && self.head.y == self.tail.y - 2 {
+            self.update_knot_position(trailing_knot_idx, tail.x + 1, tail.y - 1);
+        } else if head.x == tail.x + 1 && head.y == tail.y - 2 {
             // head up and right with a space in between
             // .H
             // ..
             // T.
-            self.update_tail_position(self.tail.x + 1, self.tail.y - 1);
-        } else if self.head.x == self.tail.x - 2 && self.head.y == self.tail.y - 1 {
+            self.update_knot_position(trailing_knot_idx, tail.x + 1, tail.y - 1);
+        } else if head.x == tail.x + 2 && head.y == tail.y - 2 {
+            // ...1
+            // ...2
+            // ....
+            // 3...
+            self.update_knot_position(trailing_knot_idx, tail.x + 1, tail.y - 1);
+        } else if head.x == tail.x - 2 && head.y == tail.y - 1 {
             // head up and left with a space in between
             // H..
             // ..T
-            self.update_tail_position(self.tail.x - 1, self.tail.y - 1);
-        } else if self.head.x == self.tail.x - 1 && self.head.y == self.tail.y - 2 {
+            self.update_knot_position(trailing_knot_idx, tail.x - 1, tail.y - 1);
+        } else if head.x == tail.x - 1 && head.y == tail.y - 2 {
             // head up and left with a space in between
             // H.
             // ..
             // .T
-            self.update_tail_position(self.tail.x - 1, self.tail.y - 1);
-        } else if self.head.x == self.tail.x && self.head.y == self.tail.y + 2 {
+            self.update_knot_position(trailing_knot_idx, tail.x - 1, tail.y - 1);
+        } else if head.x == tail.x - 2 && head.y == tail.y - 2 {
+            // 1...
+            // 2...
+            // ....
+            // ...3
+            self.update_knot_position(trailing_knot_idx, tail.x - 1, tail.y - 1);
+        } else if head.x == tail.x && head.y == tail.y + 2 {
             // head is down with one space between
             // T
             // .
             // H
-            self.update_tail_position(self.tail.x, self.tail.y + 1);
-        } else if self.head.x == self.tail.x - 2 && self.head.y == self.tail.y + 1 {
+            self.update_knot_position(trailing_knot_idx, tail.x, tail.y + 1);
+        } else if head.x == tail.x - 2 && head.y == tail.y + 1 {
             // ..T
             // H..
-            self.update_tail_position(self.tail.x - 1, self.tail.y + 1);
-        } else if self.head.x == self.tail.x - 1 && self.head.y == self.tail.y + 2 {
+            self.update_knot_position(trailing_knot_idx, tail.x - 1, tail.y + 1);
+        } else if head.x == tail.x - 1 && head.y == tail.y + 2 {
             // head down and left with a space in between
             // .T
             // ..
             // H.
-            self.update_tail_position(self.tail.x - 1, self.tail.y + 1);
-        } else if self.head.x == self.tail.x + 2 && self.head.y == self.tail.y + 1 {
+            self.update_knot_position(trailing_knot_idx, tail.x - 1, tail.y + 1);
+        } else if head.x == tail.x - 2 && head.y == tail.y + 2 {
+            // ...3
+            // ....
+            // .2..
+            // 1...
+            self.update_knot_position(trailing_knot_idx, tail.x - 1, tail.y + 1);
+        } else if head.x == tail.x + 2 && head.y == tail.y + 1 {
             // head down and right with a space in between
             // T..
             // ..H
-            self.update_tail_position(self.tail.x + 1, self.tail.y + 1);
-        } else if self.head.x == self.tail.x + 1 && self.head.y == self.tail.y + 2 {
+            self.update_knot_position(trailing_knot_idx, tail.x + 1, tail.y + 1);
+        } else if head.x == tail.x + 1 && head.y == tail.y + 2 {
             // head down and right with a space in between
             // T.
             // ..
             // .H
-            self.update_tail_position(self.tail.x + 1, self.tail.y + 1);
-        } else if self.head.x == self.tail.x - 2 && self.head.y == self.tail.y {
+            self.update_knot_position(trailing_knot_idx, tail.x + 1, tail.y + 1);
+        } else if head.x == tail.x + 2 && head.y == tail.y + 2 {
+            // 3...
+            // ....
+            // ..2.
+            // ...1
+            self.update_knot_position(trailing_knot_idx, tail.x + 1, tail.y + 1);
+        } else if head.x == tail.x - 2 && head.y == tail.y {
             // head is left with one space between
             // H.T
-            self.update_tail_position(self.tail.x - 1, self.tail.y);
-        } else if self.head.x == self.tail.x + 2 && self.head.y == self.tail.y {
+            self.update_knot_position(trailing_knot_idx, tail.x - 1, tail.y);
+        } else if head.x == tail.x + 2 && head.y == tail.y {
             // head is right with one space between
             // T.H
-            self.update_tail_position(self.tail.x + 1, self.tail.y);
+            self.update_knot_position(trailing_knot_idx, tail.x + 1, tail.y);
         }
     }
 
     fn update_rope(&mut self) {
-        if !self.planks.contains_key(&self.head) {
-            self.planks.insert(
-                self.head.clone(),
-                Plank {
-                    position: self.head.clone(),
-                    tail_visit_count: 0,
-                },
-            );
+        for idx in 0..self.knots.len() - 1 {
+            let mut head = self.knots[idx].clone();
+            let mut tail = self.knots[idx + 1].clone();
+            if !self.planks.contains_key(&head) {
+                self.planks.insert(
+                    head.clone(),
+                    Plank {
+                        position: head.clone(),
+                        tail_visit_count: 0,
+                    },
+                );
+            }
+            self.move_trailing_knot(idx + 1, &head, &tail);
         }
-        self.move_tail();
+
+        // update the tail tracker after finished
+        if let Some(tail) = self.planks.get_mut(&self.knots[self.knots.len() - 1]) {
+            //println!("before: {:?}", tail.tail_visit_count);
+            tail.tail_visit_count += 1;
+            //println!("after: {:?}", tail.tail_visit_count);
+        }
+
         //println!("{}", self);
     }
 
@@ -253,25 +321,25 @@ impl Bridge {
         match motion {
             Motion::Up { count } => {
                 for _ in 0..*count {
-                    self.head.y -= 1;
+                    self.knots[0].y -= 1;
                     self.update_rope();
                 }
             }
             Motion::Down { count } => {
                 for _ in 0..*count {
-                    self.head.y += 1;
+                    self.knots[0].y += 1;
                     self.update_rope();
                 }
             }
             Motion::Left { count } => {
                 for _ in 0..*count {
-                    self.head.x -= 1;
+                    self.knots[0].x -= 1;
                     self.update_rope();
                 }
             }
             Motion::Right { count } => {
                 for _ in 0..*count {
-                    self.head.x += 1;
+                    self.knots[0].x += 1;
                     self.update_rope();
                 }
             }
@@ -282,11 +350,13 @@ impl Bridge {
 
 impl Default for Bridge {
     fn default() -> Self {
-        let start_pos = Coordinates::from(&"(6,6)".to_string());
+        let start_pos = Coordinates::from(&"(20,20)".to_string());
+        let mut knots: Vec<Coordinates> = vec![];
+        knots.push(start_pos.clone()); // initial head
+        knots.push(start_pos.clone()); // initial tail
         let mut bridge = Bridge {
             planks: HashMap::new(),
-            head: start_pos.clone(),
-            tail: start_pos.clone(),
+            knots: knots.clone(),
         };
         bridge.planks.insert(
             start_pos.clone(),
@@ -302,22 +372,46 @@ impl Default for Bridge {
 impl fmt::Display for Bridge {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut msg = String::default();
-        for y in 0..=self.get_max_y() {
-            for x in 0..=self.get_max_x() {
-                let pos = Coordinates::from(&format!("({},{})", x, y));
-                if self.planks.contains_key(&pos) {
-                    if &pos == &self.head {
-                        msg = format!("{} H", msg);
-                    } else if &pos == &self.tail {
-                        msg = format!("{} T", msg);
+
+        let display = "tail_touch";
+
+        if display == "tail_touch" {
+            for y in 0..=self.get_max_y() {
+                for x in 0..=self.get_max_x() {
+                    let pos = Coordinates::from(&format!("({},{})", x, y));
+                    if self.planks.contains_key(&pos) {
+                        if &pos == &self.knots[0] {
+                            msg = format!("{} H", msg);
+                        } else if &pos == &self.knots[self.knots.len() - 1] {
+                            msg = format!("{} T", msg);
+                        } else {
+                            msg = format!("{} #", msg);
+                        }
                     } else {
-                        msg = format!("{} {}", msg, self.planks.get(&pos).unwrap());
+                        msg = format!("{} .", msg);
                     }
-                } else {
-                    msg = format!("{} .", msg);
                 }
+                msg = format!("{}\n", msg);
             }
-            msg = format!("{}\n", msg);
+        } else {
+            for y in 0..=self.get_max_y() {
+                for x in 0..=self.get_max_x() {
+                    let mut found = false;
+                    for knot_idx in 0..self.knots.len() {
+                        let pos = Coordinates::from(&format!("({},{})", x, y));
+                        if self.knots[knot_idx] == pos {
+                            msg = format!("{} {}", msg, knot_idx);
+                            found = true;
+
+                            break;
+                        }
+                    }
+                    if !found {
+                        msg = format!("{} .", msg);
+                    }
+                }
+                msg = format!("{}\n", msg);
+            }
         }
         write!(f, "{}", msg)
     }
