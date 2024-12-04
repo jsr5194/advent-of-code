46e431982e3823ed884d049f86136cccf1a79600
Jared Rittle <>
2024-12-04 11:43:57 -0500

    2024: prep for day 5

---
+++ 2024/aoc2024/src/exercises/day5/mod.rs
@@ -0,0 +1,41 @@
+use log::info;
+
+pub fn process_input(filedata: &String) {}
+
+pub fn run_part1(filedata: &String) -> u32 {
+    let result = 0;
+    println!("Day 5 Part 1 Result: {}", result);
+    result
+}
+
+pub fn run_part2(filedata: &String) -> u32 {
+    let result = 0;
+    println!("Day 5 Part 2 Result: {}", result);
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::common::read_file;
+    use crate::exercises::day4::run_part1;
+    use crate::exercises::day4::run_part2;
+    #[test]
+    fn test() {
+        //        assert_eq!(
+        //            run_part1(&read_file("./src/exercises/day4/input_test.txt")),
+        //            18
+        //        );
+        //        assert_eq!(
+        //            run_part1(&read_file("./src/exercises/day4/input.txt")),
+        //            2547
+        //        );
+        //        assert_eq!(
+        //            run_part2(&read_file("./src/exercises/day4/input_test.txt")),
+        //            9
+        //        );
+        //        assert_eq!(
+        //            run_part2(&read_file("./src/exercises/day4/input.txt")),
+        //            1939
+        //        )
+    }
+}
